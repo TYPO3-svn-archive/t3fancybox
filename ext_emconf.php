a0d8492b350add0d7ab2480889dae832e65c402f
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-10 23:25:38 +0000

    0.0.1 - first tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3fancybox/trunk@71021 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "test".
+ *
+ * Auto generated 10-02-2013 19:57
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'test',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Dirk Wildt',
+	'author_email' => 'wildt@die-netzmacher.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"3c77";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b65c";s:14:"ext_tables.php";s:4:"8aa8";s:16:"locallang_db.xml";s:4:"b915";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"effd";s:20:"doc/wizard_form.html";s:4:"7277";s:25:"pi1/class.tx_test_pi1.php";s:4:"a464";}',
+);
+
+?>
---
+++ ext_tables.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['types'][$_EXTKEY]['showitem'] = 'CType;;4;button;1-1-1, header;;3;;2-2-2';
+    
+$TCA['tt_content']['types'][$_EXTKEY]['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header, bodytext;Text;;richtext:rte_transform[flag=rte_enabled|mode=ts_css], rte_enabled;LLL:EXT:cms/locallang_ttc.xml:rte_enabled_formlabel, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.images, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imagefiles;imagefiles, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imagelinks;imagelinks, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.image_accessibility;image_accessibility, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.image_settings;image_settings, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imageblock;imageblock, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.textlayout;textlayout, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended';
+$TCA['tt_content']['types'][$_EXTKEY . '_pi1']['showitem'] = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.images, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imagefiles;imagefiles, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imagelinks;imagelinks, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.image_accessibility;image_accessibility, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.image_settings;image_settings, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imageblock;imageblock, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility, --palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended';
+
+
+
+t3lib_extMgm::addPlugin(array(
+    'LLL:EXT:test/locallang_db.xml:tt_content.fancyboxWithImages',
+    $_EXTKEY,
+    t3lib_extMgm::extRelPath($_EXTKEY . '_pi1') . 'ext_icon.gif'
+),'CType');
+?>
