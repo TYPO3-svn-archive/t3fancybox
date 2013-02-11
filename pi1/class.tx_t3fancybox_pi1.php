b6eb411cac5f70cba6e5e2c74f500726301dbc53
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-02-11 19:38:13 +0000

    0.0.1 - first tests
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3fancybox/trunk@71067 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_t3fancybox_pi1.php
+++ pi1/class.tx_t3fancybox_pi1.php
@@ -31,32 +31,98 @@
  * @package	TYPO3
  * @subpackage	tx_t3fancybox
  */
-class tx_t3fancybox_pi1 extends tslib_pibase {
-	public $prefixId      = 'tx_t3fancybox_pi1';		// Same as class name
-	public $scriptRelPath = 'pi1/class.tx_t3fancybox_pi1.php';	// Path to this script relative to the extension dir.
-	public $extKey        = 't3fancybox';	// The extension key.
-	public $pi_checkCHash = true;
-	
-	/**
-	 * The main method of the Plugin.
-	 *
-	 * @param string $content The Plugin content
-	 * @param array $conf The Plugin configuration
-	 * @return string The content that is displayed on the website
-	 */
-	public function main( $content, array $conf )
-        {
-          $cObj_name  = $conf['content'];
-          $cObj_conf  = $conf['content.'];
-          //$cObjData   = '<pre>' . var_export( $this->cObj->data, true ) . '</pre>';
-          unset( $this->cObj->data['image_link'] );
-          $content    = $this->cObj->cObjGetSingle( $cObj_name, $cObj_conf );
-          return $cObjData . $content;
+class tx_t3fancybox_pi1 extends tslib_pibase
+{
+    // Same as class name
+  public $prefixId      = 'tx_t3fancybox_pi1';
+    // Path to this script relative to the extension dir.
+  public $scriptRelPath = 'pi1/class.tx_t3fancybox_pi1.php';
+    // The extension key.
+  public $extKey        = 't3fancybox';	
+  
+  public $pi_checkCHash = true;
+  
+    // Current TypoSCript configuration
+  private $conf;
+  
+    // Current content
+  private $content ;
 
-          return 'Hello World!<hr />
-                  Here is the TypoScript passed to the method:' .
-                  var_export($conf, true);
-	}
+
+  
+ /**
+  * main( ) : The main method of the Plugin.
+  *
+  * @param string $content The Plugin content
+  * @param array $conf The Plugin configuration
+  * @return string The content that is displayed on the website
+  */
+  public function main( $content, array $conf )
+  {
+    $this->conf     = $conf;
+    $this->content  = $content;
+
+      // Set JavaScript code
+    $jss = $this->jss( );
+      // Handle cObj->data
+    $this->cObjData( );
+
+    $content = $this->content( );
+    return $jss . $content;
+
+//    return 'Hello World!<hr />
+//            Here is the TypoScript passed to the method:' .
+//            var_export($conf, true);
+  }
+  
+ /**
+  * jss( ) : The main method of the Plugin.
+  *
+  * @return string    $jss
+  */
+  private function jss( )
+  {
+    $jss = null;
+    
+    $imgLinks = $this->cObj->data['image_link'];
+    if( empty( $imgLinks ) )
+    {
+      return;
+    }
+    
+    $arrImgLinks = explode( PHP_EOL, $imgLinks );
+    
+    $jss = var_export( $arrImgLinks, true );
+    
+    return $jss;
+    
+    
+  }
+  
+ /**
+  * content( ) : The main method of the Plugin.
+  *
+  * @return string  $content
+  */
+  private function content( )
+  {
+    $cObj_name  = $this->conf['content'];
+    $cObj_conf  = $this->conf['content.'];
+    $content    = $this->cObj->cObjGetSingle( $cObj_name, $cObj_conf );
+    
+    return $content;
+
+  }
+  
+ /**
+  * cObjData( ) : The main method of the Plugin.
+  *
+  * @return void
+  */
+  private function cObjData( )
+  {
+    unset( $this->cObj->data['image_link'] );
+  }
 }
 
 
